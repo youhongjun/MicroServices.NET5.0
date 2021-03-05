3f6f33f10676514322411fa813cd4231f9c9d629
youhongjun <51392789+youhongjun@users.noreply.github.com>
2021-03-06 10:25:33 +1100

    SampleApiGateway

---
+++ SampleApiGateway/Payment.WebApi/Controllers/PaymentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payment.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public PaymentController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return $"This is payment service at {DateTime.Now}.";
+        }
+    }
+}
---
+++ SampleApiGateway/Product.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Product.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public ProductController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return $"This is product service at {DateTime.Now}.";
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return $"This is product {id}.";
+        }
+    }
+}
